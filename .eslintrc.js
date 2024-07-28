9425d53015e225869a50732753fcd608b7384ebb
shusaku009 <shusaku009@gmail.com>
2024-07-28 13:39:14 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "error",
+  },
+};
