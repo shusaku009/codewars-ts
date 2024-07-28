d69af87a28e57a36060e3ceb644fde5272267a52
shusaku009 <shusaku009@gmail.com>
2024-07-28 23:29:31 +0900

    add: codewars-Multiples of 3 or 5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world")
+// CodeWars Multiples of 3 or 5
+function solution(number: number): number {
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum +=;
+    }
+  }
+  return sum;
+}
