90bc7b62246c17df0a5157befc885709b59ffef9
shusaku009 <shusaku009@gmail.com>
2024-09-24 09:02:46 +0900

    add: Rock Paper Scissors

--- src/index.ts
+++ src/index.ts
@@ -233,3 +233,21 @@ export function squaresNeeded(grains: number) {
 }
 
 console.log(squaresNeeded(5));
+
+// Rock Paper Scissors
+export function rps(p1: string, p2: string): string {
+  if (p1 === p2) {
+    return "Draw";
+  }
+  if (p1 === "rock" && p2 === "scissors") {
+    return "Player 1 won!";
+  } else if (p1 === "paper" && p2 === "rock") {
+    return "Player 1 won!";
+  } else if (p1 === "scissors" && p2 === "paper") {
+    return "Player 1 won!";
+  } else {
+    return "Player 2 won!";
+  }
+}
+
+console.log(rps("rock", "paper"));
