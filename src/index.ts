3748ac3dfff41a8a144731c70e4a6d2d4c483d27
shusaku009 <shusaku009@gmail.com>
2024-12-31 14:07:07 +0900

    add: Binary sXORe

--- src/index.ts
+++ src/index.ts
@@ -526,3 +526,16 @@ export function spot(s1: string, s2: string): number[] {
   }
   return diff;
 }
+
+// Binary sXORe
+export const sxore = (n: number): number => {
+  if (n % 4 === 0) {
+    return n;
+  } else if (n % 4 === 1) {
+    return 1;
+  } else if (n % 4 === 2) {
+    return n + 1;
+  } else {
+    return 0;
+  }
+};
